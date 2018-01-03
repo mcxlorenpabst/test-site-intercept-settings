8f4b3426890511c6a81de80fa110543f02d68f4f
Loren Pabst <loren.pabst@ALLEGIANCE.LOCAL>
2018-01-03 15:52:08 -0700

    added survey invite component

---
+++ src/components/SurveyInvite/SurveyInvite.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import './SurveyInvite.css';
+
+class SurveyInvite extends Component {
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+
+    }
+
+  }
+
+  componentDidMount(){
+    
+  }
+
+  render() {
+    return (
+      <section className=''>
+        
+      </section>
+    );
+  }
+}
+
+
+export default SurveyInvite;
