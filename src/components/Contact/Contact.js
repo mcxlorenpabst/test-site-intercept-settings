fe5046a1246669aec686a4b6f857a66001034e01
Loren Pabst <loren.pabst@ALLEGIANCE.LOCAL>
2018-01-03 13:53:22 -0700

    built contact page

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,20 +1,7 @@
 import React, { Component } from 'react';
-
+import './Contact.css';
 
 class Contact extends Component {
-
-  constructor(props){
-    super(props);
-
-    this.state = {
-
-    }
-
-  }
-
-  componentDidMount(){
-    
-  }
 
   render() {
     return (
