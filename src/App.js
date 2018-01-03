4f1e4bf0d8a2d9206df98feaad136950ff7c342a
Loren Pabst <loren.pabst@ALLEGIANCE.LOCAL>
2018-01-03 15:24:57 -0700

    added settings to settings modal

--- src/App.js
+++ src/App.js
@@ -9,16 +9,33 @@ import './App.css';
 class App extends Component {
   constructor(props){
     super(props);
-
     this.state = {
-      showSettingsModal: false
+      showSettingsModal: true,
+      surveyURL: "https://alias.allegiancetech.com/cgi-bin/qwebcorporate.dll?idx=SXWD5F", 
+      showOnLoad: false, 
+      invitationID: 'mcxInviteModal', 
+      probability: 5, 
+      width: 900, 
+      height: 600, 
+      expireDaysIfYes: 84, 
+      expireDaysIfNo: 42, 
+      delay: 3000, 
+      enabled: true
     }
+
     this.toggleSettingsModal = this.toggleSettingsModal.bind(this);
+    this.closeSettingsModal = this.closeSettingsModal.bind(this);
   }
 
   toggleSettingsModal(){
     this.setState({
       showSettingsModal: !this.state.showSettingsModal
+    })
+  }
+
+  closeSettingsModal(){
+    this.setState({
+      showSettingsModal: false
     })
   }
 
@@ -28,6 +45,57 @@ class App extends Component {
 
         <Header toggleSettingsModal={this.toggleSettingsModal} />
         { router }
+
+        {
+          this.state.showSettingsModal ? 
+            <div className='settings'>
+
+              <p className='settings_close' onClick={this.closeSettingsModal} >X</p>
+
+              <div className='setting_wrapper'>
+                <p>Survey URL:</p>
+                <input className='settings_input_long' value={this.state.surveyURL} onChange={(e) => this.setState({surveyURL: e.target.value})} />
+              </div>
+
+              <div className='setting_wrapper'>
+                <p>Show On Load:</p>
+                <select value={this.state.showOnLoad} onChange={(e) => this.setState({showOnLoad: e.target.value})} >
+                  <option>true</option>
+                  <option>false</option>
+                </select>
+              </div>
+
+              <div className='setting_wrapper'>
+                <p>Probability (%):</p>
+                <input className='settings_input_number' value={this.state.probability} onChange={(e) => this.setState({probability: e.target.value})} type='number' />
+              </div>
+
+              <div className='setting_wrapper'>
+                <p>Delay (in seconds):</p>
+                <input className='settings_input_number' value={this.state.delay/1000} onChange={(e) => this.setState({delay: (e.target.value * 1000)})} type='number' />
+              </div>
+
+              <div className='setting_wrapper'>
+                <p>Enabled:</p>
+                <select value={this.state.enabled} onChange={(e) => this.setState({enabled: e.target.value})} >
+                  <option>true</option>
+                  <option>false</option>
+                </select>
+              </div>
+
+              <div className='setting_wrapper'>
+                <p>Survey Width (pixels):</p>
+                <input className='settings_input_number' value={this.state.width} onChange={(e) => this.setState({width: e.target.value})} type='number' />
+              </div>
+
+              <div className='setting_wrapper'>
+                <p>Survey Height (pixels):</p>
+                <input className='settings_input_number' value={this.state.height} onChange={(e) => this.setState({height: e.target.value})} type='number' />
+              </div>
+
+            </div>
+          : null
+        }
 
       </div>
     );
